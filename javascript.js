020b69f896324c58b1ee28b10bcd0e366f6d890b
Mark Daniels <mark_daniels1982@hotmail.com>
2015-07-03 22:13:23 -0700

    make Sarah's List and 2 squares

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// a test function to show the use of javascript
+
+var message = function() {
+  alert("Hi Sarah!");
+};
+
+message();
+
+var trial = function() {
+  console.log("hi");
+};
+
+trial();
